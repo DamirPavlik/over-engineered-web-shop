140d2438c48a119b5ed73ad51b47b0d51a5e9c7d
DamirPavlik <damir.pavlik@gmail.com>
2024-07-06 15:52:12 +0200

    feat: add configs for the whole app

---
+++ configs/container/container.php
@@ -0,0 +1,8 @@
+<?php
+
+use DI\ContainerBuilder;
+
+$containerBuilder = new ContainerBuilder();
+$containerBuilder->addDefinitions(__DIR__ . '/container_bindings.php');
+
+return $containerBuilder->build();
