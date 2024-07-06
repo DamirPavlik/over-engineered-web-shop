6679571243da4da8091244aa118a32a7da4a1370
DamirPavlik <damir.pavlik@gmail.com>
2024-07-06 15:54:09 +0200

    feat: add bootstraping for the whole app

---
+++ bootstrap.php
@@ -0,0 +1,21 @@
+<?php
+
+use Dotenv\Dotenv;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/configs/path_constants.php';
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$container      = require CONFIG_PATH . '/container/container.php';
+$addMiddlewares = require CONFIG_PATH . '/middleware.php';
+
+AppFactory::setContainer($container);
+
+$app = AppFactory::create();
+
+$addMiddlewares($app);
+
+return $app;
