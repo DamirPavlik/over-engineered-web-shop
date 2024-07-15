93969df2d3b07952a9a6538b71154beef521e3b6
DamirPavlik <damir.pavlik@gmail.com>
2024-07-15 22:53:00 +0200

    feat: add admin routes to web.php

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Controllers\AdminController;
 use App\Controllers\HomeController;
+use App\Middleware\AdminMiddleware;
 use Slim\App;
+use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-    $app->get('/', [HomeController::class, 'index']);
+    $app->group('', function(RouteCollectorProxy $group) {
+        $group->get('/', [HomeController::class, 'index']);
+
+        $group->group('/admin-dashboard', function(RouteCollectorProxy $adminDashboard) {
+            $adminDashboard->get('/login', [AdminController::class, 'renderLogin']);
+            $adminDashboard->post('/login/submit', [AdminController::class, 'login']);
+            $adminDashboard->get('', [AdminController::class, 'index'])->add(AdminMiddleware::class);
+            $adminDashboard->get('/users', [AdminController::class, 'renderUsers'])->add(AdminMiddleware::class);
+            $adminDashboard->get('/products', [AdminController::class, 'renderProducts'])->add(AdminMiddleware::class);
+        });
+    });
 };
