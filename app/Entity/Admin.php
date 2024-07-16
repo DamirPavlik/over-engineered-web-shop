e8d2b7e93459da5e5be6b0e74152556a3f2a70da
DamirPavlik <damir.pavlik@gmail.com>
2024-07-16 23:50:39 +0200

    feat: add admin entity

---
+++ app/Entity/Admin.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use App\Contracts\AdminEntityInterface;
+use App\Entity\Traits\HasTimestamps;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity, Table('admins')]
+#[HasLifecycleCallbacks]
+class Admin implements AdminEntityInterface
+{
+    use HasTimestamps;
+
+    #[Id, Column(options: ['unsigned' => true]), GeneratedValue]
+    private int $id;
+
+    #[Column]
+    private string $name;
+
+    #[Column]
+    private string $email;
+
+    #[Column]
+    private string $password;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+}
