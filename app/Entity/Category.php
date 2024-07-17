158b3a87bdf315a0b11ccc5d9d0fd1160230f772
DamirPavlik <damir.pavlik@gmail.com>
2024-07-17 17:49:42 +0200

    feat: add category entity

---
+++ app/Entity/Category.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Entity;
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity, Table('categories')]
+class Category
+{
+    #[Id, Column(options: ['unsigned' => true]), GeneratedValue]
+    private int $id;
+
+    #[Column]
+    private string $name;
+
+    #[OneToMany(targetEntity: Product::class, mappedBy: "categories")]
+    private Collection $products;
+
+    public function __construct()
+    {
+        $this->products = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getProducts(): Collection
+    {
+        return $this->products;
+    }
+
+    public function addProducts(Product $products): Category
+    {
+        $this->products->add($products);
+        return $this;
+    }
+}
