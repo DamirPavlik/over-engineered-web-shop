22f307cc92697da284a2ae4439bbc3e526c58a4d
DamirPavlik <damir.pavlik@gmail.com>
2024-07-22 00:24:38 +0200

    feat: idk added a bunch of stuff to routes

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -1,21 +1,39 @@
 <?php
 
 use App\Controllers\AdminController;
+use App\Controllers\CategoryController;
 use App\Controllers\HomeController;
+use App\Controllers\ProductController;
+use App\Controllers\UserController;
 use App\Middleware\AdminMiddleware;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-    $app->group('', function(RouteCollectorProxy $group) {
-        $group->get('/', [HomeController::class, 'index']);
+    // Public routes
+    $app->get('/', [HomeController::class, 'index']);
 
-        $group->group('/admin-dashboard', function(RouteCollectorProxy $adminDashboard) {
-            $adminDashboard->get('/login', [AdminController::class, 'renderLogin']);
-            $adminDashboard->post('/login/submit', [AdminController::class, 'login']);
-            $adminDashboard->get('', [AdminController::class, 'index'])->add(AdminMiddleware::class);
-            $adminDashboard->get('/users', [AdminController::class, 'renderUsers'])->add(AdminMiddleware::class);
-            $adminDashboard->get('/products', [AdminController::class, 'renderProducts'])->add(AdminMiddleware::class);
-        });
+    // Admin routes
+    $app->group('/admin-dashboard', function(RouteCollectorProxy $adminDashboard) {
+        // Login routes
+        $adminDashboard->get('/login', [AdminController::class, 'renderLogin']);
+        $adminDashboard->post('/login', [AdminController::class, 'login']);
+
+        // Protected admin routes
+        $adminDashboard->group('', function(RouteCollectorProxy $adminProtected) {
+            $adminProtected->get('', [AdminController::class, 'index']);
+            $adminProtected->post('/logout', [AdminController::class, 'logout']);
+
+            $adminProtected->get('/products', [ProductController::class, 'index']);
+            $adminProtected->post('/products', [ProductController::class, 'addProduct']);
+            $adminProtected->get('/products/load', [ProductController::class, 'load']);
+
+            $adminProtected->get('/categories', [CategoryController::class, 'index']);
+            $adminProtected->post('/categories', [CategoryController::class, 'addCategory']);
+            $adminProtected->get('/categories/load', [CategoryController::class, 'load']);
+
+            $adminProtected->get('/users', [UserController::class, 'index']);
+            $adminProtected->post('/users', [UserController::class, 'addUser']);
+        })->add(AdminMiddleware::class);
     });
 };
