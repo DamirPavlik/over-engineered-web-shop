137b56832998b5fa26318561264ffa2f9481e2b1
DamirPavlik <damir.pavlik@gmail.com>
2024-07-28 22:24:12 +0200

    feat: add a bunch of middlewares to middleware.php

--- configs/middleware.php
+++ configs/middleware.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Config;
+use App\Middleware\OldFormDataMiddleware;
+use App\Middleware\StartSessionMiddleware;
+use App\Middleware\ValidationErrorsMiddleware;
+use App\Middleware\ValidationExceptionMiddleware;
 use Slim\App;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
@@ -9,10 +13,13 @@ return function (App $app) {
     $container = $app->getContainer();
     $config    = $container->get(Config::class);
 
-    // Twig
     $app->add(TwigMiddleware::create($app, $container->get(Twig::class)));
+    $app->add(ValidationExceptionMiddleware::class);
+    $app->add(ValidationErrorsMiddleware::class);
+    $app->add(OldFormDataMiddleware::class);
+    $app->add(StartSessionMiddleware::class);
 
-    // Logger
+    $app->addBodyParsingMiddleware();
     $app->addErrorMiddleware(
         (bool) $config->get('display_error_details'),
         (bool) $config->get('log_errors'),
