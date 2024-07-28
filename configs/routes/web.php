8eec144b9faddbe5e8adb852f0f52428997d8dbd
DamirPavlik <damir.pavlik@gmail.com>
2024-07-28 22:20:47 +0200

    feat: add a bunch of new routes and refactor by groups

--- configs/routes/web.php
+++ configs/routes/web.php
@@ -10,30 +10,40 @@ use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-    // Public routes
     $app->get('/', [HomeController::class, 'index']);
 
-    // Admin routes
     $app->group('/admin-dashboard', function(RouteCollectorProxy $adminDashboard) {
-        // Login routes
         $adminDashboard->get('/login', [AdminController::class, 'renderLogin']);
         $adminDashboard->post('/login', [AdminController::class, 'login']);
 
-        // Protected admin routes
         $adminDashboard->group('', function(RouteCollectorProxy $adminProtected) {
             $adminProtected->get('', [AdminController::class, 'index']);
             $adminProtected->post('/logout', [AdminController::class, 'logout']);
+            $adminProtected->get('/admin/load', [AdminController::class, 'load']);
+            $adminProtected->delete('/admin/{admin}', [AdminController::class, 'delete']);
 
-            $adminProtected->get('/products', [ProductController::class, 'index']);
-            $adminProtected->post('/products', [ProductController::class, 'addProduct']);
-            $adminProtected->get('/products/load', [ProductController::class, 'load']);
+            $adminProtected->group('/products', function(RouteCollectorProxy $products) {
+                $products->get('', [ProductController::class, 'index']);
+                $products->post('', [ProductController::class, 'addProduct']);
+                $products->get('/load', [ProductController::class, 'load']);
+                $products->delete('/{product}', [ProductController::class, 'delete']);
+                $products->post('/update/{product}', [ProductController::class, 'update']);
+            });
 
-            $adminProtected->get('/categories', [CategoryController::class, 'index']);
-            $adminProtected->post('/categories', [CategoryController::class, 'addCategory']);
-            $adminProtected->get('/categories/load', [CategoryController::class, 'load']);
+            $adminProtected->group('/categories', function(RouteCollectorProxy $categories) {
+                $categories->get('', [CategoryController::class, 'index']);
+                $categories->post('', [CategoryController::class, 'addCategory']);
+                $categories->get('/load', [CategoryController::class, 'load']);
+                $categories->delete('/{category}', [CategoryController::class, 'delete']);
+            });
 
-            $adminProtected->get('/users', [UserController::class, 'index']);
-            $adminProtected->post('/users', [UserController::class, 'addUser']);
+            $adminProtected->group('/users', function(RouteCollectorProxy $users) {
+                $users->get('', [UserController::class, 'index']);
+                $users->post('', [UserController::class, 'addUser']);
+                $users->get('/load', [UserController::class, 'load']);
+                $users->delete('/{user}', [UserController::class, 'delete']);
+            });
+
         })->add(AdminMiddleware::class);
     });
 };
