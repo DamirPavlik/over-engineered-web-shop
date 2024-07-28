fcb557be3eda0adfceedbcb974cf5ae080cc15f1
DamirPavlik <damir.pavlik@gmail.com>
2024-07-28 22:22:18 +0200

    feat: add category id to response from get all and add update method to product service

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Contracts\EntityManagerServiceInterface;
+use App\DataObjects\ProductData;
 use App\Entity\Category;
 use App\Entity\Product;
 
@@ -36,15 +37,41 @@ class ProductService
         try {
             $queryBuilder = $this->entityManager->getRepository(Product::class)
                 ->createQueryBuilder('p')
-                ->select('p')
+                ->leftJoin('p.category', 'c')
+                ->select('p.id', 'p.name', 'p.description', 'p.price', 'p.stockQuantity', 'p.createdAt', 'p.updatedAt', 'c.id AS category_id')
                 ->getQuery();
 
-            return $queryBuilder->getArrayResult();
+            $results = $queryBuilder->getArrayResult();
+
+            return array_map(function($product) {
+                return [
+                    'id' => $product['id'],
+                    'name' => $product['name'],
+                    'description' => $product['description'],
+                    'price' => $product['price'],
+                    'stockQuantity' => $product['stockQuantity'],
+                    'createdAt' => $product['createdAt'],
+                    'updatedAt' => $product['updatedAt'],
+                    'category_id' => $product['category_id'],
+                ];
+            }, $results);
         } catch (\Exception $e) {
-            return ["error" => $e];
+            return ["error" => $e->getMessage()];
         }
     }
 
+    public function update(Product $product, ProductData $productData): Product
+    {
+        $categoryRepository = $this->entityManager->getRepository(Category::class);
+        $category = $categoryRepository->find($productData->categoryId);
+        $product->setName($productData->name);
+        $product->setDescription($productData->description);
+        $product->setPrice($productData->price);
+        $product->setStockQuantity($productData->stockQuantity);
+        $product->setCategory($category);
+
+        return $product;
+    }
 
 
 }
