c7d0994d92dd9a5528288e979e85fd6e66d28f4d
DamirPavlik <damir.pavlik@gmail.com>
2024-07-29 22:47:10 +0200

    feat: add edit functionality to categories

--- resources/js/category.js
+++ resources/js/category.js
@@ -17,11 +17,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 const actionsTd = document.createElement("td");
                 // Add any actions you want, like edit or delete buttons
-                actionsTd.innerHTML = `<button>Edit</button> <button>Delete</button>`;
+                actionsTd.innerHTML = `<button class="edit-category admin-edit-button" data-category='${JSON.stringify(category)}'>Edit <i class="bi bi-pencil"></i></button> <button data-id="${category.id}" data-name="${data.name}" class="delete-category admin-delete-button">Delete <i class="bi bi-trash"></i></button>`;
                 tr.appendChild(actionsTd);
 
                 categoriesWrapper.appendChild(tr);
             });
         })
         .catch(error => console.error('Error fetching categories:', error));
+
+    const popup = document.querySelector('#edit-popup');
+    const popupForm = document.querySelector('#edit-popup form');
+    const closePopupBtn = document.querySelector('#closePopupBtn');
+
+    document.addEventListener("click", e => {
+        if (e.target.classList.contains('delete-category')) {
+
+            e.preventDefault();
+
+            const categoryId = e.target.getAttribute('data-id');
+            const categoryName = e.target.getAttribute('data-name');
+            const confirmPrompt = confirm(`Are you sure you want to delete ${categoryName}`);
+
+            if (confirmPrompt) {
+                fetch(`/admin-dashboard/categories/${categoryId}`, {
+                    method: "DELETE"
+                }).then(data => {
+                    window.location.reload();
+                });
+            }
+        }
+
+        if (e.target.classList.contains('edit-category')) {
+            const category = JSON.parse(e.target.getAttribute('data-category'));
+            console.log("category: ",  JSON.parse(e.target.getAttribute('data-category')));
+
+            document.querySelector('#edit-popup #name').value = category.name;
+            popupForm.setAttribute("action", `/admin-dashboard/categories/update/${category.id}`);
+            popup.style.display = 'flex';
+        }
+
+        if (e.target.classList.contains('close') || e.target === popup) {
+            popup.style.display = 'none';
+        }
+    });
+
+    closePopupBtn.addEventListener('click', () => {
+        popup.style.display = 'none';
+    });
 });
